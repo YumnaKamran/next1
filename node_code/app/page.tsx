df106919c6cdda5a1629912b5bf4df0179b420a1
YumnaKamran <yumnakamran1998@gmail.com>
2024-05-03 13:25:02 -0700

    executed

---
+++ node_code/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <h1>yumna kamran</h1>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum esse aut molestiae fuga ullam 
+        tempora vero! Tempore dolore atque, voluptas accusamus optio eaque quos blanditiis, fugiat 
+        error est assumenda delectus!</p>
+    </main>
+  );
+}
